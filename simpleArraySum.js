d1a53a99d53fcad6dfb0992c1763db5cddee3dc9
Alex Franco <alex.franco9@icloud.com>
2022-06-16 21:37:09 -0500

    simple array sum added

---
+++ simpleArraySum.js
@@ -0,0 +1,18 @@
+// Given an array of integers, find the sum of its elements.
+// For example, if the array arr = [1,2,3], 1+2+3 = 6, so return 6.
+// Function Description
+// Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+// simpleArraySum has the following parameter(s):
+// arr : an array of integers
+
+
+function simpleArraySum(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
+}
+
+console.log(simpleArraySum([1,2,3]));
+console.log(simpleArraySum([1,2,3,4,10,11]));
